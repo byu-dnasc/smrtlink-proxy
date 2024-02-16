c3af8c42be3be8444df58ba472c46455d6329feb
Ammon Knaupp <adknaupp@gmail.com>
2024-02-16 11:43:23 -0700

    Configure nginx for unprivileged execution

---
+++ README.md
@@ -0,0 +1,14 @@
+# Installation
+
+### Download
+
+The smrtlink-repository must be cloned to your user's home directory
+
+### Setup
+
+The container image must be built by the same user who will run it. So, execute
+the following commands as the user who will end up running it:
+
+1. Load bash functions into your shell with `. manage.sh`
+2. run `build`
+3. run `start`
--- manage.sh
+++ manage.sh
@@ -1,13 +1,40 @@
 build() {
-    singularity build --fakeroot \
+    singularity build \
+        --fakeroot \
+        --force \
+        --build-arg user=$USER \
         alpine_nginx.sif alpine_nginx.def
 }
 
 start() {
-    singularity instance start \ 
-        alpine_nginx.sif proxy
+    container && echo "The container is already running." && return 1
+    singularity instance start alpine_nginx.sif proxy
 }
 
 stop() {
+    ! container && echo "The container is not running." && return 1
     singularity instance stop proxy
+}
+
+container() { singularity instance list | grep -q proxy; }
+web() { curl -s -o /dev/null localhost:8088; }
+
+shell() {
+    ! container && echo "The container is not running." && return 1
+    singularity shell instance://proxy
+}
+
+swap_config() {
+    [ -z $1 ] && echo "Usage: set_config <path_to_config>" && return 1
+    [ ! -f $1 ] && echo "File not found: $1. File must be in your home directory, or else built into the container." && return
+    # Test the configuration
+    singularity exec instance://proxy \
+        /usr/sbin/nginx -t -c $1
+    # If the test is successful, set the configuration
+    if [ $? -eq 0 ]; then
+        singularity exec instance://proxy \
+            /usr/sbin/nginx -c $1
+    else
+        echo "Nginx configuration test failed"
+    fi
 }

493cce57a13cd0096f20c0e788dddb08c1ede1ff
Ammon Knaupp <adknaupp@gmail.com>
2024-02-16 15:12:52 -0700

    Move nginx prefix from definition to manage.sh

--- manage.sh
+++ manage.sh
@@ -1,8 +1,10 @@
+NGINX_PREFIX="/home/$USER/smrtlink-proxy/nginx"
+
 build() {
     singularity build \
         --fakeroot \
         --force \
-        --build-arg user=$USER \
+        --build-arg nginx_prefix=$NGINX_PREFIX \
         alpine_nginx.sif alpine_nginx.def
 }
 
@@ -29,11 +31,11 @@ swap_config() {
     [ ! -f $1 ] && echo "File not found: $1. File must be in your home directory, or else built into the container." && return
     # Test the configuration
     singularity exec instance://proxy \
-        /usr/sbin/nginx -t -c $1
+        nginx -p $NGINX_PREFIX -t -c $1
     # If the test is successful, set the configuration
     if [ $? -eq 0 ]; then
         singularity exec instance://proxy \
-            /usr/sbin/nginx -c $1
+            nginx -p $NGINX_PREFIX -c $1
     else
         echo "Nginx configuration test failed"
     fi

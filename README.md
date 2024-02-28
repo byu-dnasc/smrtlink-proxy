5f5657714d7a54b51a88faa49a31ce3aa53a7797
Ammon Knaupp <adknaupp@gmail.com>
2024-02-28 10:58:40 -0700

    Add template testing

--- README.md
+++ README.md
@@ -12,3 +12,19 @@ the following commands as the user who will end up running it:
 1. Load bash functions into your shell with `. manage.sh`
 2. run `build`
 3. run `start`
+
+### NGINX configuration templates
+
+The NGINX configuration files essentially make up smrtlink-proxy.
+These files begin as templates containing references to variables.
+These variable references must be replaced with the variable's value.
+
+This substitution can occur when the container image is built, but
+during template development, it is more convenient to use the `test_template`
+function to make config files from the templates. This command also
+feeds the generated config to NGINX for validation.
+
+### Troubleshooting
+
+Serveral bash functions exist in `manage.sh` for troubleshooting smrtlink-proxy's
+level of functionality. These include `web`, `http`, `https`.
--- manage.sh
+++ manage.sh
@@ -1,12 +1,23 @@
 REPO_ROOT="/home/$USER/smrtlink-proxy"
 SMRT_ROOT="/home/$USER/smrtlink-container/smrtlink"
+PROXY_CONF="/etc/nginx/proxy.conf"
+
 NGINX_CMD="nginx -p $REPO_ROOT/nginx -c /etc/nginx/unprivileged.conf"
 
+build_configs() {
+    for t in "$REPO_ROOT"/templates/*; do
+        template=/tmp/$(basename "$t").conf
+        cp "$t" $template
+        for var in REPO_ROOT SMRT_ROOT PROXY_CONF; do
+            sed -i "s|$var|${!var}|g" $template
+        done
+    done
+}
+
 build() {
+    build_configs
     singularity build \
         --fakeroot --force \
-        --build-arg repo_root="$REPO_ROOT" \
-        --build-arg smrt_root="$SMRT_ROOT" \
         --build-arg nginx_cmd="$NGINX_CMD" \
         alpine_nginx.sif alpine_nginx.def
 }
@@ -22,21 +33,31 @@ stop() {
 }
 
 container() { singularity instance list | awk 'NR>1 {print $1}' | grep -q proxy; }
-web() { curl -s localhost:8088 > /dev/null; }
-http() { curl -s http://localhost:9092/status > /dev/null; }
-https() { test "$(curl -s -k -o /dev/null -w '%{http_code}' https://localhost:8244/SMRTLink/1.0.0/status)" -eq 401; }
+sl() { curl -s -o /dev/null http://localhost:9091/status; }
+web() { curl -s -o /dev/null localhost:8088; }
+http() {
+    ! container && echo "The container is not running." && return 1
+    ! sl && echo "SMRT Link is not accessible." && return 1
+    test "$(curl -s -o /dev/null -w '%{http_code}' http://localhost:9092/status)" -eq 200;
+}
+https() { 
+    ! container && echo "The container is not running." && return 1
+    ! sl && echo "SMRT Link is not accessible." && return 1
+    test "$(curl -s -k -o /dev/null -w '%{http_code}' https://localhost:8244/SMRTLink/1.0.0/status)" -eq 401; 
+}
 
 shell() {
     ! container && echo "The container is not running." && return 1
     singularity shell instance://proxy
 }
 
-reload() {
-    ! container && echo "The container is not running." && return 1
-    singularity exec instance://proxy eval "$NGINX_CMD -s reload"
-}
-
-config() {
+get_config() {
     ! container && echo "The container is not running." && return 1
     singularity exec instance://proxy eval "$NGINX_CMD -T"
+}
+
+test_templates() {
+    ! container && echo "The container is not running." && return 1
+    ( PROXY_CONF=/tmp/proxy.conf; build_configs )
+    singularity exec instance://proxy nginx -p $REPO_ROOT/nginx -t -c /tmp/unprivileged.conf
 }
